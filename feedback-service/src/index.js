db14049346f8470cdbe300cfad1038759f3bd952
justin tang <justintang@justins-MacBook-Pro.local>
2020-01-22 14:25:30 +0800

    setup db and project structure

---
+++ feedback-service/src/index.js
@@ -0,0 +1,9 @@
+import "@babel/polyfill";
+import "dotenv/config";
+
+import "#root/db/connection";
+import "#root/server/startServer";
+
+// import path from "path";
+
+// console.log("working!!!")
